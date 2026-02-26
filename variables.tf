14a48df7673c3175d99ee61968dfab9f16b47a4a
Sonu Kumar <ersonukumar19@gmail.com>
2026-02-26 17:44:40 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "region" { default = "us-east-1" }
+variable "environment" { default = "uat" }
+variable "vpc_cidr" { default = "10.0.0.0/16" }
+variable "instance_type" { default = "t3.micro" }
+variable "db_instance_class" { default = "db.t3.micro" }
